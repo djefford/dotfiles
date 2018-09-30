fe2cd615c97b9ecd958210832ec0f9e48c2655ef
djefford <djefford58@gmail.com>
2018-09-29 22:04:55 -0400

    Initialized file

---
+++ files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+############################################################
+# Author: Dustin Jefford
+# Description:
+#		Script to manage the dotfiles being tracked in this
+#		repository.
+############################################################
+
+dotfiles = ('
